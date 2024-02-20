27cd6c08c363f093fc30fafaac80d07449620354
Mariana Sofii <Mariana.Sofii@simcorp.com>
2024-02-20 17:33:13 +0200

    homework1 - solution

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Homework2 {
+
+    enum TransactionType { Buy, Sell }
+
     class Program {
+
         static void Main(string[] args) {
+            Console.WriteLine("Input the nominal of the trade:");
+            string userInputNom = Console.ReadLine();
+            int nominal = Int32.Parse(userInputNom);
+
+            Console.WriteLine("Input the price of the trade:");
+            string userInputPrice = Console.ReadLine();
+            int price = Int32.Parse(userInputPrice);
+
+            Console.WriteLine("Input the transaction code of the trade:");
+            TransactionType trcType;
+            string  userInputTrans = Console.ReadLine();
+            trcType = (TransactionType)Enum.Parse(typeof(TransactionType), userInputTrans, true);
+
+            int sign = trcType == TransactionType.Buy ? 1 : -1;
+
+            int curVal = sign * nominal * price;
+
+            Console.WriteLine($"Current value ={curVal}");
+
+            Console.ReadKey();
         }
     }
 }
