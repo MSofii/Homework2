f123b9f512e11cc7f463705ee25ecd656c0ae08c
Mariana Sofii <Mariana.Sofii@simcorp.com>
2024-02-20 17:43:18 +0200

    added part 2

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -16,6 +16,10 @@ namespace Homework2 {
             string userInputPrice = Console.ReadLine();
             int price = Int32.Parse(userInputPrice);
 
+            Console.WriteLine("Input the original price of the trade:");
+            string userInputPriceOrigin = Console.ReadLine();
+            int priceOrigin = Int32.Parse(userInputPriceOrigin);
+
             Console.WriteLine("Input the transaction code of the trade:");
             TransactionType trcType;
             string  userInputTrans = Console.ReadLine();
@@ -23,9 +27,15 @@ namespace Homework2 {
 
             int sign = trcType == TransactionType.Buy ? 1 : -1;
 
+            int factor = trcType == TransactionType.Buy ? 0 : 1;
+
             int curVal = sign * nominal * price;
 
             Console.WriteLine($"Current value ={curVal}");
+            
+            int pL = (price - priceOrigin)*nominal*factor;
+            Console.WriteLine($"PL equals {pL}");
+
 
             Console.ReadKey();
         }
